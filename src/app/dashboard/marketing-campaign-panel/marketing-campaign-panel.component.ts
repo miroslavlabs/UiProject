d8126abf45fe79ad25d1f3ae07682047f3e46bfb
Miroslav Shtarbev <Miroslav Shtarbev>
2018-01-20 16:35:15 +0200

    Added middle label in doughnut chart for the Marketing Campaign panel.

--- src/app/dashboard/marketing-campaign-panel/marketing-campaign-panel.component.ts
+++ src/app/dashboard/marketing-campaign-panel/marketing-campaign-panel.component.ts
@@ -26,13 +26,34 @@ export class MarketingCampaignPanelComponent {
                 }]
         };
 
+        var dougnutMiddleLableDisplayFunction = function() {
+            var chartInstance = this.chart.chart.chart;
+            var width = chartInstance.width;
+            var height = chartInstance.height;
+
+            chartInstance.ctx.font = '900 60px Lato';
+            chartInstance.ctx.textBaseline = 'middle';            
+            chartInstance.ctx.fillStyle = '#343a41';
+
+            var text = "MC";              
+            var textX = Math.round((width - chartInstance.ctx.measureText(text).width) / 2);
+            var textY = (height + chartInstance.controller.legend.height) / 2;
+
+            chartInstance.ctx.fillText(text, textX, textY);
+        };
+
         this.options = {
             responsive: true,
             maintainAspectRatio: false,
             legend: {
                 display: true,
             },
-            cutoutPercentage: 70
-        };
+            cutoutPercentage: 70,
+            animation: {
+                duration: 100,
+                onComplete: dougnutMiddleLableDisplayFunction,
+                onProgress: dougnutMiddleLableDisplayFunction
+            }
+        }
     }
 }
